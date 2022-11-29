d5666a512c8cd801db4c314e5b15433bef316332
Jaelson dos santos freitas <jaelson.jaelsonsantos.freitas@gmail.com>
2022-11-29 20:36:46 -0300

    Criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClassejava.java
@@ -0,0 +1,13 @@
+package cursojava.executavel;
+
+public class PrimeiraClassejava {
+	
+	/*Main é um metado auto executavel em java*/
+	public static void main(String[] args) {
+		
+		/*Escreve texto no console*/
+		System.out.println("Meu primeiro programa em java");
+		System.out.println("java é orientado a objeto");
+	}
+
+}
