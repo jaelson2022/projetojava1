b184e0588d74eedc7d00fee1c3e280011d8d0814
Jaelson dos santos freitas <jaelson.jaelsonsantos.freitas@gmail.com>
2022-11-30 21:56:16 -0300

    ddddd

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClassejava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClassejava.java
@@ -2,11 +2,12 @@ package cursojava.executavel;
 
 public class PrimeiraClassejava {
 	
-	/*Main ddddddddd é um metado auto executavel em java*/
+	/*Main ddfffff é um metado auto executavel em java*/
 	public static void main(String[] args) {
 		
 		/*Escreve texto no console*/
 		System.out.println("Meu primeiro programa em java");
+		System.out.println("java é orientado a objeto");
 		System.out.println("java é orientado a objeto");
 	}
 
