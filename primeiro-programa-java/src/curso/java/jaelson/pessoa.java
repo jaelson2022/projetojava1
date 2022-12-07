b135f3f1a611b22c63a575a9ba827c4f7730e6f8
Jaelson dos santos freitas <jaelson.jaelsonsantos.freitas@gmail.com>
2022-12-06 22:15:35 -0300

    Alteração e criação da classe pessoa

---
+++ primeiro-programa-java/src/curso/java/jaelson/pessoa.java
@@ -0,0 +1,27 @@
+package curso.java.jaelson;
+
+public class pessoa {
+	
+	private string nome;
+	
+	private string sobrenome;
+
+	public string getNome() {
+		return nome;
+	}
+
+	public void setNome(string nome) {
+		this.nome = nome;
+	}
+
+	public string getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(string sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	
+
+}
